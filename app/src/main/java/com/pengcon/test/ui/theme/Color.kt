6b4118eacd10e99db3b3f932c61abd6f5748986e
pengcon <vpdzjs@gmail.com>
2024-11-03 00:33:45 +0900

    13ww

--- app/src/main/java/com/pengcon/test/ui/theme/Color.kt
+++ app/src/main/java/com/pengcon/test/ui/theme/Color.kt
@@ -1,3 +1,4 @@
+
 package com.pengcon.test.ui.theme
 
 import androidx.compose.ui.graphics.Color
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
