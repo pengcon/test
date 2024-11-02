15e0473ac22aabbf1b409ffec0f6d905b60744a8
pengcon <vpdzjs@gmail.com>
2024-11-03 00:13:27 +0900

    13ww

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,22 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.ktlint.gradle)
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android = true
+        ignoreFailures = false
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.SARIF)
+            reporter(ReporterType.HTML)
+        }
+    }
 }
